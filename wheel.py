9f87eae3c6cb33e9797a8a2408f92a479c443b62
Jessica Calkins <jessicacalkins795213@gmail.com>
2022-01-27 15:59:20 -0600

    random pizz

--- wheel.py
+++ wheel.py
@@ -1,4 +1,9 @@
 import random
 
-places = ["chicken express", "panda express", "taco casa", "taco bell", "whataburer", "kfc", "subway", "pizza", "slims chicken", "zaxby's", "in n out", ""]
-print(random.choice(places))
+places = ["chicken express", "panda express", "taco casa", "taco bell", "whataburer", "kfc", "subway", "pizza", "slims chicken", "zaxby's", "in n out", "wingstop"]
+random1 = random.choice(places)
+if random1 == "pizza":
+    pizz_places=["Pizza Hut", "Dominos", "Little Cesars", "Marcos Pizza", "Mr.Jims", "Jimmy's Pizza Pasta and subs"]
+    print(random.choice(pizz_places))
+else:
+    print(random1)
