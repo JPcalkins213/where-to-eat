d8517100c4cfc11463e5cd3e025733d152ac5e1a
Jessica Calkins <jessicacalkins795213@gmail.com>
2022-01-27 15:59:06 -0600

    if pizza is chosen itll randomly choose where

---
+++ wheel.py
@@ -0,0 +1,4 @@
+import random
+
+places = ["chicken express", "panda express", "taco casa", "taco bell", "whataburer", "kfc", "subway", "pizza", "slims chicken", "zaxby's", "in n out", ""]
+print(random.choice(places))
