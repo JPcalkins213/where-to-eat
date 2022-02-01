1678b0555dfa9d1ca8eff596692959a542768e5c
Jessica Calkins <jessicacalkins795213@gmail.com>
2022-01-31 21:17:07 -0600

    forgot to add chicken last time

--- wheel.py
+++ wheel.py
@@ -4,7 +4,7 @@ print("Would you like to choose the category?")
 choice1 = input("yes or no: ")
 if choice1 == "yes":
     print("what category would you like?")
-    choice2=input("Asian,  Mexican, Burgers, Healthy, Pizza?: ")
+    choice2=input("Asian,  Mexican, Burgers, Healthy, Pizza, Wings/chicken?: ")
     if choice2 == "Asian" or choice2 == "asian":
         asian = ["Wongs Express", "Panda Express", "Adorianas"]
         print(random.choice(asian))
@@ -20,6 +20,10 @@ if choice1 == "yes":
     if choice2 == "Pizza" or choice2 == "pizza":
         pizza=["Pizza Hut", "Dominos", "Marcos Pizza", "Mr.Jims"]
         print(random.choice(pizza))
+    if choice2 == "wings" or choice2 == "chicken" or choice2 == "Wings" or choice2 == "Chicken" or choice2 == "wings/chicken" or choice2 == "Wings/Chicken":
+        wings = ["Zaxby's", "Wingstop", "Slims Chicken", "Canes", "chicken express", "chick fila", "Buffalo Wild Wings", "kfc"]
+        print(random.choice(wings))
+
 else:
     places = ["Asian", "Mexican", "Burgers", "Healthy", "pizza", "Wings/chicken"]
     random1 = random.choice(places)
@@ -29,7 +33,7 @@ else:
         print(random.choice(pizza))
     if random1 == "Burgers":
         print(random1)
-        burgers = ["whataburger", "pops burger", "burger king", "in n out"]
+        burgers = ["whataburger", "pops burger", "burger king", "in n out",]
         print(random.choice(burgers))
     if random1 == "Wings/chicken":
         print(random1)
