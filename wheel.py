45a2116621872ebd55a428995f8b7c6d6ed25d9b
Jessica Calkins <jessicacalkins795213@gmail.com>
2022-02-24 15:18:17 -0600

    cleaned up the code

--- wheel.py
+++ wheel.py
@@ -1,5 +1,13 @@
 import random
 
+asian = ["Wongs Express", "Panda Express", "Adorianas"]
+mexican = ["Tio Tony's", "287 Tacos", "La Salsa Verde", "Taco Bell", "Taco Casa", "On the boarder", "El Fenix"]
+burgers = ["whataburger", "pops burger", "burger king", "in n out"]
+healthy = ["Subway", "Solata", "Mod Pizza", "Jersey Mike's Subs"]
+pizza= ["Pizza Hut", "Dominos", "Marcos Pizza", "Mr.Jims"]
+wings = ["Zaxby's", "Wingstop", "Slims Chicken", "Canes", "chicken express", "chick fila", "Buffalo Wild Wings", "kfc"]
+places = ["Asian", "Mexican", "Burgers", "Healthy", "pizza", "Wings/chicken"]
+
 print("Would you like to pick a preset category [1]? or would you like to insert you own[2]? ")
 choice = input("1 or 2: ") 
 if choice == "2":
@@ -21,50 +29,29 @@ else:
         print("what category would you like?")
         choice2=input("Asian,  Mexican, Burgers, Healthy, Pizza, Wings/chicken?: ")
         if choice2 == "Asian" or choice2 == "asian":
-            asian = ["Wongs Express", "Panda Express", "Adorianas"]
             print(random.choice(asian))
-        if choice2 == "Mexican" or choice2 == "mexican":
-            mexican = ["Tio Tony's", "287 Tacos", "La Salsa Verde", "Taco Bell", "Taco Casa", "On the boarder", "El Fenix"]
+        elif choice2 == "Mexican" or choice2 == "mexican":
             print(random.choice(mexican))
-        if choice2 == "Burgers" or choice2 == "burgers":
-            burgers = ["whataburger", "pops burger", "burger king", "in n out"]
+        elif choice2 == "Burgers" or choice2 == "burgers":
             print(random.choice(burgers))
-        if choice2 == "Healthy" or choice2 == "healthy":
-            healthy = ["Subway", "Solata", "Mod Pizza", "Jersey Mike's Subs"]
+        elif choice2 == "Healthy" or choice2 == "healthy":
             print(random.choice(healthy))
-        if choice2 == "Pizza" or choice2 == "pizza":
-            pizza=["Pizza Hut", "Dominos", "Marcos Pizza", "Mr.Jims"]
+        elif choice2 == "Pizza" or choice2 == "pizza":
             print(random.choice(pizza))
-        if choice2 == "wings" or choice2 == "chicken" or choice2 == "Wings" or choice2 == "Chicken" or choice2 == "wings/chicken" or choice2 == "Wings/Chicken":
-            wings = ["Zaxby's", "Wingstop", "Slims Chicken", "Canes", "chicken express", "chick fila", "Buffalo Wild Wings", "kfc"]
+        else:
             print(random.choice(wings))
 
     else:
-        places = ["Asian", "Mexican", "Burgers", "Healthy", "pizza", "Wings/chicken"]
         random1 = random.choice(places)
-        if random1 == "pizza":
-            print(random1)
-            pizza=["Pizza Hut", "Dominos", "Marcos Pizza", "Mr.Jims"]
-            print(random.choice(pizza))
-        if random1 == "Burgers":
-            print(random1)
-            burgers = ["whataburger", "pops burger", "burger king", "in n out",]
-            print(random.choice(burgers))
-        if random1 == "Wings/chicken":
-            print(random1)
-            wings = ["Zaxby's", "Wingstop", "Slims Chicken", "Canes", "chicken express", "chick fila", "Buffalo Wild Wings", "kfc"]
-            print(random.choice(wings))
-        if random1 == "Asian":
-            print(random1)
-            asian = ["Wongs Express", "Panda Express", "Adorianas"]
-            print(random.choice(asian))
-        if random1 == "Mexican":
-            print(random1)
-            mexican = ["Tio Tony's", "287 Tacos", "La Salsa Verde", "Taco Bell", "Taco Casa", "On the boarder", "El Fenix", "la tapitios", "Uncle Julios"]
-            print(random.choice(mexican))
-        if random1 == "Healthy":
-            print(random1)
-            healthy = ["Subway", "Solata", "Mod Pizza", "Jersey Mike's Subs"]
-            print(random.choice(healthy))
-        else:
-            print("no food for you")
+        if random1 == places[4]:
+            print(f'{random1}\n{random.choice(pizza)}')
+        elif random1 == places[2]:
+            print(f'{random1}\n{random.choice(burgers)}')
+        elif random1 == places[5]:
+            print(f'{random1}\n{random.choice(wings)}')
+        elif random1 == places[0]:
+            print(f'{random1}\n{random.choice(asian)}')
+        elif random1 == places[1]:
+            print(f'{random1}\n{random.choice(mexican)}')
+        elif random1 == places[3]:
+            print(f'{random1}\n{random.choice(healthy)}')
